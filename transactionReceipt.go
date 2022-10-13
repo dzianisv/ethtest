ab29686e6db460f7b2fbe7acb080cf3dde0ec98a
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-13 20:21:55 +0200

    Run concurrent requests

--- transactionReceipt.go
+++ transactionReceipt.go
@@ -12,18 +12,18 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-func main() {
-	name := os.Getenv("NAME")
-	password := os.Getenv("PASSWORD")
-	host := os.Getenv("HOST")
-	url := fmt.Sprintf("https://%s:%s@%s", name, password, host)
+type Response struct {
+	I   int
+	Err error
+}
 
-	log.Printf("Using %s\n", url)
-
+func query(url string, i int, c chan Response) {
 	rpcClient, err := rpc.DialHTTP(url)
 
 	if err != nil {
-		log.Fatalf("Failed to create a RPC client: %v", err)
+		log.Printf("[%d] Failed to create a RPC client: %v", i, err)
+		c <- Response{i, err}
+
 	}
 
 	client := ethclient.NewClient(rpcClient)
@@ -31,11 +31,43 @@ func main() {
 	hash := "0x75d714f13cad3b57aa240ae1f3a2a91873c994b622e582a7e19a8757d157f299"
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
 	defer cancel()
-	transactionReceipt, err := client.TransactionReceipt(ctx, common.HexToHash(hash))
+	_, err = client.TransactionReceipt(ctx, common.HexToHash(hash))
 
 	if err != nil {
-		log.Fatalf("Failed to query: %v", err)
+		log.Printf("[%d] Failed to query : %v", i, err)
+		c <- Response{i, nil}
 	}
 
-	log.Println(transactionReceipt)
+	// log.Printf("[%d] Response %v", i, transactionReceipt)
+	c <- Response{i, nil}
+}
+
+func main() {
+	name := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+	host := os.Getenv("HOST")
+	url := fmt.Sprintf("https://%s:%s@%s", name, password, host)
+	log.Printf("Using %s\n", url)
+
+	c := make(chan Response)
+	count := 100000
+	concurrency := 100
+	active := 0
+
+	for {
+		if active < concurrency && count > 0 {
+			go query(url, count, c)
+			count -= 1
+			active += 1
+		} else if active > 0 {
+			response := <-c
+			if response.Err != nil {
+				log.Printf("[%d] %v", response.I, response.Err)
+			}
+			active -= 1
+		} else {
+			log.Printf("Request finished")
+			break
+		}
+	}
 }
