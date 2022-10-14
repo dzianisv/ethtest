210a45342e0e6f1e42263d0d8fa54cebd2b0057c
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-14 15:30:50 +0300

    Print delays. Use different user agent

--- transactionReceipt.go
+++ transactionReceipt.go
@@ -14,32 +14,45 @@ import (
 )
 
 type Response struct {
-	I   int
-	Err error
+	I         int
+	Err       error
+	DelayMili int64
 }
 
 func query(url string, i int, c chan Response) (*types.Receipt, error) {
 	rpcClient, err := rpc.DialHTTP(url)
 
 	if err != nil {
-		c <- Response{i, err}
+		c <- Response{i, err, 0}
 		return nil, err
 	}
+
+	userAgent := fmt.Sprintf("ethtest %d %s", i, time.Now().Format(time.RFC3339))
+	rpcClient.SetHeader("User-Agent", userAgent)
 
 	client := ethclient.NewClient(rpcClient)
 
 	hash := "0x75d714f13cad3b57aa240ae1f3a2a91873c994b622e582a7e19a8757d157f299"
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
 	defer cancel()
+	start_t := time.Now()
 	receipt, err := client.TransactionReceipt(ctx, common.HexToHash(hash))
+	delay_ms := time.Now().UnixMilli() - start_t.UnixMilli()
 
 	if err != nil {
-		c <- Response{i, err}
+		c <- Response{i, err, delay_ms}
 		return nil, err
 	}
 
-	c <- Response{i, nil}
+	c <- Response{i, nil, delay_ms}
 	return receipt, nil
+}
+
+func Max(x, y int64) int64 {
+	if x < y {
+		return y
+	}
+	return x
 }
 
 func main() {
@@ -50,27 +63,43 @@ func main() {
 	log.Printf("Using %s\n", url)
 
 	c := make(chan Response)
-	request_n := 100000
+	request_n := 50000
 	count := request_n
 	concurrency := 100
 	active_n := 0
 	errors_n := 0
+	latency := make([]int64, request_n)
 
 	for {
 		if active_n < concurrency && count > 0 {
-			go query(url, count, c)
 			count -= 1
 			active_n += 1
+			go query(url, count, c)
 		} else if active_n > 0 {
 			response := <-c
+			latency[response.I] = response.DelayMili
+
 			if response.Err != nil {
 				errors_n += 1
 				log.Printf("[%d] Error: %s; active: %d\n", response.I, response.Err, active_n)
 			}
 			active_n -= 1
 		} else {
-			log.Printf("%d/%d failed\n", errors_n, request_n)
 			break
 		}
 	}
+
+	max_delay_ms := latency[0]
+	sum_ms := int64(0)
+
+	for i, delay_ms := range latency {
+		max_delay_ms = Max(delay_ms, max_delay_ms)
+		sum_ms += delay_ms
+		log.Printf("%d\t%d\n", i, delay_ms)
+	}
+
+	avg_ms := sum_ms / int64(request_n)
+	log.Printf("Max delay %d ms", max_delay_ms)
+	log.Printf("Average delay %d ms", avg_ms)
+	log.Printf("%d/%d failed\n", errors_n, request_n)
 }
