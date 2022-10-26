23d55087bf9ce23506b8706001aabbfca61d26a5
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-26 11:02:52 +0300

    Add basic authorization token

--- benchmarks/h2load.sh
+++ benchmarks/h2load.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-
-exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load' --data data.json  "https://$NAME:$PASSWORD@$HOST/"
+#!/bin/bash
+set -euo pipefail
+# apt install -yq nghttp2-client
+BASE64_USER_PASSWORD=$(echo "$NAME:$PASSWORD" | base64)
+exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load' --data data.json  -H "Authorization: Basic $BASE64_USER_PASSWORD" "https://$HOST/"
