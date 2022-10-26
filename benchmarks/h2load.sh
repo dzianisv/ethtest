9715105c49acf612df5992d359db2a2d896d9a8c
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-26 12:44:33 +0300

    Remove new lines

--- benchmarks/h2load.sh
+++ benchmarks/h2load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # apt install -yq nghttp2-client
-exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load'  -H "Authorization: Basic $(echo -n $NAME:$PASSWORD | base64)" --data data.json "https://$HOST/"
+exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load'  -H "Authorization: Basic $(echo -n $NAME:$PASSWORD | base64 | tr -d '\n')" --data data.json "https://$HOST/"
