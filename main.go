176125ebfd7e2edaa90240585b00fc12bffa85e3
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-26 19:17:50 +0300

    Fix breakdown

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ func percentileBreakdown(data []int64) ([]BreakdownEntry, int64, int64) {
 	}
 
 	for i, breakdownEntry := range breakdowns {
-		breakdowns[i].lessThan = (int64(i)+1)*divider + divider
+		breakdowns[i].lessThan = (int64(i) + 1) * divider
 		breakdowns[i].percenntage = float32(breakdownEntry.count) * 100 / float32(len(data))
 	}
 
