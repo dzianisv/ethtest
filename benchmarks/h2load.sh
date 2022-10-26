ab4a7ba0c0729e77e639f82aa949223e8e9c69e4
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-10-26 11:15:59 +0300

    Update h2load.sh

--- benchmarks/h2load.sh
+++ benchmarks/h2load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 # apt install -yq nghttp2-client
-BASE64_USER_PASSWORD=$(echo "$NAME:$PASSWORD" | base64)
-exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load' --data data.json  -H "Authorization: Basic $BASE64_USER_PASSWORD" "https://$HOST/"
+BASE64_USER_PASSWORD=$(echo -n "$NAME:$PASSWORD" | base64)
+exec h2load -n 1000000 -c 100 -H "Content-Type: application/json" -H 'User-Agent: h2load'  -H "Authorization: Basic $BASE64_USER_PASSWORD" --data data.json "https://$HOST/"
